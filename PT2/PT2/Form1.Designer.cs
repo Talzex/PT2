0795af29f27ea24e8d5b438c7a35887c29aee823
t.halley@numericable.fr <t.halley@numericable.fr>
2021-06-04 16:54:41 +0200

    Correction Erreur

--- PT2/PT2/Form1.Designer.cs
+++ PT2/PT2/Form1.Designer.cs
@@ -29,273 +29,96 @@ namespace PT2
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.Connexion = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBoxMotDePasse = new System.Windows.Forms.TextBox();
-            this.Login = new System.Windows.Forms.Label();
-            this.textBoxLogin = new System.Windows.Forms.TextBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.supp = new System.Windows.Forms.Button();
+            this.ajouter = new System.Windows.Forms.Button();
+            this.emprunt = new System.Windows.Forms.Button();
+            this.retourne = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // listBox1
             // 
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.Connexion);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBoxMotDePasse);
-            this.panel1.Controls.Add(this.Login);
-            this.panel1.Controls.Add(this.textBoxLogin);
-            this.panel1.Location = new System.Drawing.Point(58, 25);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(333, 380);
-            this.panel1.TabIndex = 0;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(375, 95);
+            this.listBox1.TabIndex = 0;
             // 
-            // Connexion
+            // listBox2
             // 
-            this.Connexion.Location = new System.Drawing.Point(125, 330);
-            this.Connexion.Name = "Connexion";
-            this.Connexion.Size = new System.Drawing.Size(75, 23);
-            this.Connexion.TabIndex = 5;
-            this.Connexion.Text = "Connexion";
-            this.Connexion.UseVisualStyleBackColor = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 168);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(375, 95);
+            this.listBox2.TabIndex = 1;
             // 
-            // label2
+            // supp
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 210);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Mot de passe";
+            this.supp.Location = new System.Drawing.Point(393, 12);
+            this.supp.Name = "supp";
+            this.supp.Size = new System.Drawing.Size(75, 23);
+            this.supp.TabIndex = 2;
+            this.supp.Text = "Supprimer";
+            this.supp.UseVisualStyleBackColor = true;
+            this.supp.Click += new System.EventHandler(this.supp_Click);
             // 
-            // textBoxMotDePasse
+            // ajouter
             // 
-            this.textBoxMotDePasse.Location = new System.Drawing.Point(38, 226);
-            this.textBoxMotDePasse.Name = "textBoxMotDePasse";
-            this.textBoxMotDePasse.Size = new System.Drawing.Size(100, 20);
-            this.textBoxMotDePasse.TabIndex = 2;
+            this.ajouter.Location = new System.Drawing.Point(393, 84);
+            this.ajouter.Name = "ajouter";
+            this.ajouter.Size = new System.Drawing.Size(75, 23);
+            this.ajouter.TabIndex = 3;
+            this.ajouter.Text = "Ajouter Abonné";
+            this.ajouter.UseVisualStyleBackColor = true;
+            this.ajouter.Click += new System.EventHandler(this.ajouter_Click);
             // 
-            // Login
+            // emprunt
             // 
-            this.Login.AutoSize = true;
-            this.Login.Location = new System.Drawing.Point(35, 137);
-            this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(33, 13);
-            this.Login.TabIndex = 1;
-            this.Login.Text = "Login";
-            this.Login.Click += new System.EventHandler(this.Login_Click);
+            this.emprunt.Location = new System.Drawing.Point(12, 269);
+            this.emprunt.Name = "emprunt";
+            this.emprunt.Size = new System.Drawing.Size(75, 23);
+            this.emprunt.TabIndex = 4;
+            this.emprunt.Text = "Emprunter";
+            this.emprunt.UseVisualStyleBackColor = true;
+            this.emprunt.Click += new System.EventHandler(this.emprunt_Click);
             // 
-            // textBoxLogin
+            // retourne
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(38, 153);
-            this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(229, 20);
-            this.textBoxLogin.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.textBox6);
-            this.panel2.Controls.Add(this.label8);
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Controls.Add(this.textBox4);
-            this.panel2.Controls.Add(this.textBox5);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.textBox3);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(438, 25);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(340, 380);
-            this.panel2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(140, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Inscrivez-vous";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 21);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Connectez-vous";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(143, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Inscription";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(54, 226);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Mot de passe";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(57, 242);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 9;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(54, 49);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Nom";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(57, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(229, 20);
-            this.textBox2.TabIndex = 7;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(54, 93);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(43, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Prénom";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(57, 109);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(229, 20);
-            this.textBox3.TabIndex = 12;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(54, 137);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(30, 13);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "Pays";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(57, 153);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(229, 20);
-            this.textBox4.TabIndex = 14;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(54, 180);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(33, 13);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "Login";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(57, 196);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(229, 20);
-            this.textBox5.TabIndex = 16;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(54, 273);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(118, 13);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "Confirmer Mot de passe";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(57, 289);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 18;
+            this.retourne.Location = new System.Drawing.Point(119, 269);
+            this.retourne.Name = "retourne";
+            this.retourne.Size = new System.Drawing.Size(75, 23);
+            this.retourne.TabIndex = 5;
+            this.retourne.Text = "Retourner";
+            this.retourne.UseVisualStyleBackColor = true;
+            this.retourne.Click += new System.EventHandler(this.retourne_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.retourne);
+            this.Controls.Add(this.emprunt);
+            this.Controls.Add(this.ajouter);
+            this.Controls.Add(this.supp);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxMotDePasse;
-        private System.Windows.Forms.Label Login;
-        private System.Windows.Forms.TextBox textBoxLogin;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button Connexion;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button supp;
+        private System.Windows.Forms.Button ajouter;
+        private System.Windows.Forms.Button emprunt;
+        private System.Windows.Forms.Button retourne;
     }
 }
 
--- PT2/PT2/Form1.cs
+++ PT2/PT2/Form1.cs
@@ -12,9 +12,93 @@ namespace PT2
 {
     public partial class Form1 : Form
     {
+        MusiquePT2_MEntities musique;
         public Form1()
         {
+
             InitializeComponent();
+            musique = new MusiquePT2_MEntities();
+            chargerListBoxAbonnees();
+            chargerListBoxEmprunter();
+        }
+
+        private void chargerListBoxAbonnees()
+        {
+            var abonne = (from j in musique.ABONNÉS
+                          select j).ToList();
+            listBox1.Items.Clear();
+            foreach (ABONNÉS j in abonne)
+            {
+                listBox1.Items.Add(j);
+            }
+        }
+
+        private void chargerListBoxEmprunter()
+        {
+            var joueurs = (from j in musique.EMPRUNTER
+                           select j.ALBUMS.TITRE_ALBUM).ToList();
+            listBox2.Items.Clear();
+            foreach (String j in joueurs)
+            {
+                listBox2.Items.Add(j);
+            }
+        }
+
+        private void ajouter_Click(object sender, EventArgs e)
+        {
+            ABONNÉS a = new ABONNÉS();
+            a.NOM_ABONNÉ = "Duthil";
+            a.PRÉNOM_ABONNÉ = "Thomas";
+            a.LOGIN_ABONNÉ = "tduthil";
+            a.PASSWORD_ABONNÉ = "12345";
+
+            musique.ABONNÉS.Add(a);
+            musique.SaveChanges();
+            chargerListBoxAbonnees();
+        }
+
+        private void supp_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                ABONNÉS j = (ABONNÉS)listBox1.SelectedItem;
+                musique.ABONNÉS.Remove(j);
+                musique.SaveChanges();
+                chargerListBoxAbonnees();
+            }
+        }
+
+        private void retourne_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                String titremprunt = (String)listBox2.SelectedItem;
+                var emprunt = from a1 in musique.EMPRUNTER
+                              where a1.ALBUMS.TITRE_ALBUM == titremprunt
+                              select a1;
+                musique.EMPRUNTER.Remove(emprunt.First());
+                musique.SaveChanges();
+                chargerListBoxEmprunter();
+            }
+        }
+
+        private void emprunt_Click(object sender, EventArgs e)
+        {
+            EMPRUNTER emprunt = new EMPRUNTER();
+            ABONNÉS j = (ABONNÉS)listBox1.SelectedItem;
+
+            emprunt.CODE_ABONNÉ = j.CODE_ABONNÉ;
+            emprunt.DATE_EMPRUNT = DateTime.Now;
+            emprunt.CODE_ALBUM = 15;
+            var delaiAlbum = from a2 in musique.ALBUMS
+                             where a2.CODE_ALBUM == 15
+                             join p in musique.GENRES
+                             on a2.CODE_GENRE equals p.CODE_GENRE
+                             select p.DÉLAI;
+            emprunt.DATE_RETOUR_ATTENDUE = DateTime.Now.AddDays((double)delaiAlbum.First());
+            musique.EMPRUNTER.Add(emprunt);
+            musique.SaveChanges();
+            chargerListBoxEmprunter();
         }
 
         private void Login_Click(object sender, EventArgs e)
