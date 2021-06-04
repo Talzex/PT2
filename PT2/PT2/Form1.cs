e62f5d67586046504457dc49e488fd662a06845e
nathan moussiron <nathan.moussiron@etu.u-bordeaux.fr>
2021-06-04 15:40:02 +0200

    consultation emprunt

--- PT2/PT2/Form1.cs
+++ PT2/PT2/Form1.cs
@@ -12,6 +12,8 @@ namespace PT2
 {
     public partial class Form1 : Form
     {
+        MusiquePT2_MEntities musiquePT2_MEntities = new MusiquePT2_MEntities();
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,20 @@ namespace PT2
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void ConsultEmprunt()
+        {
+            var emprunt = from alb in musiquePT2_MEntities.ALBUMS
+                          join e in musiquePT2_MEntities.EMPRUNTER
+                          on alb.CODE_ALBUM equals e.CODE_ALBUM
+                          where e.CODE_ABONNÉ = musiquePT2_MEntities.ABONNÉS.CODE_ABONNÉ
+                          orderby e.DATE_RETOUR_ATTENDUE
+                          select alb;
+            foreach(EMPRUNTER e in emprunt)
+            {
+                Console.WriteLine(musiquePT2_MEntities.ALBUMS.TITRE_ALBUM + " " + e.DATE_RETOUR_ATTENDUE);
+            }
         }
     }
 }
