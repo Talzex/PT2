8b95b6e19ee8ea6e45dc9c73c999d502b9edc71f
Duthil Thomas <thomas.duthil@etu.u-bordeaux.fr>
2021-06-05 14:55:07 +0200

    Correction Erreur US2

--- PT2/PT2/Form1.cs
+++ PT2/PT2/Form1.cs
@@ -12,11 +12,12 @@ namespace PT2
 {
     public partial class Form1 : Form
     {
-        MusiquePT2_MEntities musiquePT2_MEntities = new MusiquePT2_MEntities();
+        MusiquePT2_MEntities musique = new MusiquePT2_MEntities();
 
         public Form1()
         {
             InitializeComponent();
+            ConsultEmprunt();
         }
 
         private void Login_Click(object sender, EventArgs e)
@@ -31,26 +32,15 @@ namespace PT2
 
         public void ConsultEmprunt()
         {
-            var emprunt = from alb in musiquePT2_MEntities.ALBUMS
-                          join e in musiquePT2_MEntities.EMPRUNTER
+            var emprunt = from alb in musique.ALBUMS
+                          join e in musique.EMPRUNTER
                           on alb.CODE_ALBUM equals e.CODE_ALBUM
-                          where e.CODE_ABONNÉ == musiquePT2_MEntities.ABONNÉS.CODE_ABONNÉ
+                          where e.CODE_ABONNÉ == 27
                           orderby e.DATE_RETOUR_ATTENDUE
                           select alb;
-            foreach(EMPRUNTER e in emprunt)
+            foreach(ALBUMS e in emprunt)
             {
-                Console.WriteLine(musiquePT2_MEntities.ALBUMS.TITRE_ALBUM + " " + e.DATE_RETOUR_ATTENDUE);
-            }
-        }
-
-        public void ConsultEmpruntProlongé()
-        {
-            var empruntProlongé = from e in musiquePT2_MEntities.EMPRUNTER
-                                  where e.ALLONGE = true
-                                  select e;
-            foreach(ALBUMS a in empruntProlongé)
-            {
-                Console.WriteLine(a.TITRE_ALBUM);
+                MessageBox.Show(e.TITRE_ALBUM);
             }
         }
     }
