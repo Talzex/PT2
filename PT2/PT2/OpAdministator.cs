ce6ed4d34bc065e6840d7ef83364ec84fd690477
Duthil Thomas <thomas.duthil@etu.u-bordeaux.fr>
2021-06-08 23:13:33 +0200

    Correction Erreur, Reste à Corriger US8

--- PT2/PT2/OpAdministator.cs
+++ PT2/PT2/OpAdministator.cs
@@ -9,7 +9,10 @@ namespace PT2
     class OpAdministator
     {
         MusiquePT2_MEntities musique;
+        List<EMPRUNTER> empruntsprol;
         List<EMPRUNTER> empruntsNonRapportes;
+        List<ALBUMS> albumsnonemprunte;
+        List<String> topalbums;
 
         public OpAdministator(MusiquePT2_MEntities musique)
         {
@@ -22,18 +25,18 @@ namespace PT2
 
         public List<EMPRUNTER> EmpruntProlonge()
         {
-            List<EMPRUNTER> emprunts = new List<EMPRUNTER>();
+             empruntsprol = new List<EMPRUNTER>();
             var emprunt = (from j in musique.EMPRUNTER
                            select j).ToList();
             foreach (EMPRUNTER j in emprunt)
             {
                 if (Prolonge(j))
                 {
-                    emprunts.Add(j);
+                    empruntsprol.Add(j);
                 }
 
             }
-            return emprunts;
+            return empruntsprol;
         }
 
         public void Purge()
@@ -41,7 +44,7 @@ namespace PT2
             var abo = from a in musique.ABONNÉS
                       join emp in musique.EMPRUNTER
                       on a.CODE_ABONNÉ equals emp.CODE_ABONNÉ
-                      where DateTime.Now.Subtract(emp.DATE_EMPRUNT).TotalDays >= 365
+                      where DateTime.Now.Subtract(emp.DATE_EMPRUNT).TotalDays >= 365//A changer ne fonctionn pas
                       select a;
             foreach (ABONNÉS a in abo)
             {
@@ -65,6 +68,47 @@ namespace PT2
                 }
             }
             return empruntsNonRapportes;
+        }
+
+        public List<ALBUMS> AlbumsNonEmprunte()
+        {
+            albumsnonemprunte = new List<ALBUMS>();
+            var albumNonEmpruntesDepuisUnAn = (from a in musique.ALBUMS
+                                               join emp in musique.EMPRUNTER
+                                               on a.CODE_ALBUM equals emp.CODE_ALBUM
+                                               where DateTime.Now.Subtract(emp.DATE_EMPRUNT).TotalDays >= 365//A changer ne fonctionne pas
+                                               select a);
+            if (albumNonEmpruntesDepuisUnAn.Count() != 0)
+            {
+                foreach (ALBUMS a in albumNonEmpruntesDepuisUnAn)
+                {
+                    albumsnonemprunte.Add(a);
+                }
+            }
+            return albumsnonemprunte;
+        }
+
+        public List<String> TopAlbums()
+        {
+            topalbums = new List<String>();
+            var topAlbum = (from al in musique.ALBUMS
+                           join e4 in musique.EMPRUNTER on al.CODE_ALBUM equals e4.CODE_ALBUM
+                           where e4.DATE_EMPRUNT.Year == DateTime.Now.Year
+                           group al by al.TITRE_ALBUM into g
+                           orderby g.Count() descending
+                           select new
+                           {
+                               nb = g.Count(),
+                               Name = g.Key
+                           }).Take(10);
+            if (topAlbum.Count() != 0)
+            {
+                foreach (var p in topAlbum)
+                {
+                    topalbums.Add(p.Name);
+                }
+            }
+            return topalbums;
         }
     }
 }
