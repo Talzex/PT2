db87d4f7b84aa17102a53803a8c183d6aa6e3e2a
Duthil Thomas <thomas.duthil@etu.u-bordeaux.fr>
2021-06-10 16:52:45 +0200

    test US1,2,3 La suppression ne fonctionne pas

---
+++ PT2/PT2/Outils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT2
+{
+    public class Outils
+    {
+        MusiquePT2_MEntities musique = new MusiquePT2_MEntities();
+        public void suppAbo(ABONNÉS abo)
+        {
+            musique.ABONNÉS.Remove(abo);
+            musique.SaveChanges();
+        }
+
+        public void suppEmprunt(EMPRUNTER emp)
+        {
+            musique.EMPRUNTER.Remove(emp);
+            musique.SaveChanges();
+        }
+
+    }
+}
