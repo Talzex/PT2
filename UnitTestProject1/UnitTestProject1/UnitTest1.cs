b28b2812b7ae2d4d0b5f274245228991a5245241
Duthil Thomas <thomas.duthil@etu.u-bordeaux.fr>
2021-06-11 13:23:24 +0200

    TEST US1/US2

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using PT2;
+using System.Data.Entity;
 
 namespace UnitTestProject1
 {
@@ -11,7 +12,7 @@ namespace UnitTestProject1
     {
         MusiquePT2_MEntities musique = new MusiquePT2_MEntities();
         PT2.OpAbonne opu;
-
+        /*J'ai tenté de supprimer mes emprunts et mes abonnées mais malgré l'aide  d'un professeur je n'ai pas réussi à les supprimer */
         [TestMethod]
         public void InscriptionEmprunte()
         {
@@ -24,6 +25,7 @@ namespace UnitTestProject1
             {
                 opu = new OpAbonne("TESTUS1", "TESTUS1", "testus1", "123456789", "123456789", "", "", musique);
                 opu.ajoutAbonne();
+                musique.SaveChanges();
                 
             }
             var selectcréation = (from abo in musique.ABONNÉS
@@ -32,10 +34,8 @@ namespace UnitTestProject1
             var selectcodeAbo = (from abo in musique.ABONNÉS
                                  where abo.LOGIN_ABONNÉ == "testus1"
                                  select abo.CODE_ABONNÉ).ToList();
-
-            Assert.AreEqual(1, selectcréation.Count());
-            outils.suppAbo(selectcréation.First());
-
+            Assert.IsTrue(1 == selectcréation.Count());
+            
             var selectEmprunt = (from al in musique.ALBUMS
                                  where al.TITRE_ALBUM == "Bach J-C: Sinfonien"
                                  select al).ToList();
@@ -46,7 +46,9 @@ namespace UnitTestProject1
                                         where emp.CODE_ABONNÉ == selectcodeAbo.FirstOrDefault()
                                         select emp).ToList();
                 Assert.IsTrue(selectEmpruntAbo.First().DATE_EMPRUNT != null);
+                //outils.suppAbo(selectcréation.First());
             }
+
         }
         
         [TestMethod]
@@ -89,62 +91,10 @@ namespace UnitTestProject1
 
                 /*foreach(EMPRUNTER e in selectEmpruntAbo)
                 {
-                    outils.suppEmprunt(e);
+                    outils.suppEmprunt(e, selectcréation.First());
                 }
                 outils.suppAbo(selectcréation.First());*/
             }
-            
-            
         }
-
-        [TestMethod]
-        public void ProlongeEmprunt()
-        {
-            OpUSER user = new OpUSER(musique);
-            OpAdministator admin = new OpAdministator(musique);
-            Outils outils = new Outils();
-            List<EMPRUNTER> empruntsprol = new List<EMPRUNTER>();
-            var selectnewAbo = (from abo in musique.ABONNÉS
-                                where abo.LOGIN_ABONNÉ == "testus4"
-                                select abo).ToList();
-            if (selectnewAbo.Count() == 0)
-            {
-                opu = new OpAbonne("TESTUS4", "TESTUS4", "testus4", "124", "124", "", "", musique);
-                opu.ajoutAbonne();
-
-            }
-            var selectcréation = (from abo in musique.ABONNÉS
-                                  where abo.LOGIN_ABONNÉ == "testus4"
-                                  select abo).ToList();
-            var selectEmprunt = (from al in musique.ALBUMS
-                                 where al.TITRE_ALBUM == "Bach J-C: Sinfonien"
-                                 select al).ToList();
-            var selectEmprunt2 = (from al in musique.ALBUMS
-                                  where al.TITRE_ALBUM == "Bach: Symphonien"
-                                  select al).ToList();
-            var selectcodeAbo = (from abo in musique.ABONNÉS
-                                 where abo.LOGIN_ABONNÉ == "testus2"
-                                 select abo.CODE_ABONNÉ).ToList();
-            var selectEmpruntAbo = (from emp in musique.EMPRUNTER
-                                    where emp.CODE_ABONNÉ == selectcodeAbo.FirstOrDefault()
-                                    select emp).ToList();
-            EMPRUNTER premieremprunt = user.emprunte(selectEmprunt.First(), selectcréation.First());
-            EMPRUNTER deuxpemprunt = user.emprunte(selectEmprunt2.First(), selectcréation.First());
-
-            user.prolongation(premieremprunt);
-            user.prolongation(deuxpemprunt);
-
-            empruntsprol = admin.EmpruntProlonge();
-            Assert.IsTrue(empruntsprol.Contains(premieremprunt) && empruntsprol.Contains(deuxpemprunt));
-            /*foreach (EMPRUNTER e in selectEmpruntAbo)
-            {
-                outils.suppEmprunt(e);
-            }
-            outils.suppAbo(selectcréation.First());*/
-
-        }
-
-
-
     }
 }
